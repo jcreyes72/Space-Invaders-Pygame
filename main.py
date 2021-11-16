2e2166a03d507caf3ce736b1fbecb5f50a2950da
rhay-repo <65616653+rhay-repo@users.noreply.github.com>
2021-11-15 23:37:17 -0500

    laser.wav -> newlaser.wav
    updated the laser sound effect with the new .wav file

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ while running:
                 playerX_change = 5
             if event.key == pygame.K_SPACE:
                 if bullet_state is "ready":
-                    bulletSound = mixer.Sound("laser.wav")
+                    bulletSound = mixer.Sound("newlaser.wav")
                     bulletSound.play()
                     # Get the current x cordinate of the spaceship
                     bulletX = playerX
