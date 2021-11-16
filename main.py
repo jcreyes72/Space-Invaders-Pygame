d92b61e18a60a7c59590d4b00dcb779748bdc9f5
MarkTAus <78713767+MarkTAus@users.noreply.github.com>
2021-11-15 21:19:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 
 # Background
-background = pygame.image.load('background.png')
+background = pygame.image.load('altbackground.png')
 
 # Sound
 mixer.music.load("newbackground.wav")
